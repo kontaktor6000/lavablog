dd97b19af4a68f4c0eac3463605dc22b1224222b
serhio-vasilenko <promservis.sale@gmail.com>
2019-12-30 05:49:49 +0200

    added model

---
+++ src/app/Http/Models/Parfume.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Parfume extends Model
+{
+
+    public $table = 'parfume';
+
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 
+use App\Http\Controllers\ParfumeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/parfume', [ParfumeController::class, 'indexAction']);
+
+Route::post('parfume', [ParfumeController::class, 'getDataAction']);
